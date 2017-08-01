4351150417ed67a59e0773e7e5882971f2160c3b
Alan Sax <saxal28@gmail.com>
2017-07-31 23:27:44 -0500

    added bootstrap

--- src/react/sections/HomeSection.jsx
+++ src/react/sections/HomeSection.jsx
@@ -7,7 +7,7 @@ export class HomeSection extends React.Component {
 
 				<section className="home">
 					<div className="container">
-						<h1>Book A <strong>Princess</strong> For Your Next Party</h1>
+						<h1>Book A <strong>Princess</strong> <br />For Your Next Party</h1>
 						<div className="button-row">
 							<button>Gallery</button>
 							<button className="active">Book Now</button>
@@ -16,7 +16,51 @@ export class HomeSection extends React.Component {
 				</section>
 
 				<section className="about">
-					<h1><strong>Who We Are</strong></h1>
+					<div className="container full-height full-width">
+						<div className="col-sm-6 full-height center-vertical-horizontal primary-theme">
+							<h1>Who We Are</h1>
+							<p>Happily Ever After STL specializes in providing princesses
+								and disney characters to parties and events.
+							</p>
+							<button>Book Now</button>
+						</div>
+						<div className="col-sm-6 full-height no-padding image-col">
+						</div>
+					</div>
+				</section>
+
+					{/*<section className="categories">*/}
+						{/*<div className="container full-width full-height">*/}
+							{/*<div className="col-sm-4 full-height goldenrod">*/}
+								{/*<div className="category-box">*/}
+									{/*<h1>Cast</h1>*/}
+								{/*</div>*/}
+							{/*</div>*/}
+							{/*<div className="col-sm-4 full-height goldenrod">*/}
+								{/*<div className="category-box">*/}
+								{/*<h1>Cast</h1>*/}
+								{/*</div>*/}
+							{/*</div>*/}
+							{/*<div className="col-sm-4 full-height goldenrod">*/}
+								{/*<div className="category-box">*/}
+								{/*<h1>Cast</h1>*/}
+								{/*</div>*/}
+							{/*</div>*/}
+						{/*</div>*/}
+					{/*</section>*/}
+
+				<section className="about">
+					<div className="container full-height full-width">
+						<div className="col-sm-6 full-height center-vertical-horizontal primary-theme-inverted">
+							<h1>Who We Are</h1>
+							<p>Happily Ever After STL specializes in providing princesses
+								and disney characters to parties and events.
+							</p>
+							<button>Book Now</button>
+						</div>
+						<div className="col-sm-6 full-height no-padding primary-theme">
+						</div>
+					</div>
 				</section>
 
 			</div>
